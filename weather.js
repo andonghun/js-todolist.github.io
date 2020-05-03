5505e471bdc7b7076f0b1d9a13fdf257c6dde6d5
andonghun <37131996+andonghun@users.noreply.github.com>
2020-05-04 02:18:39 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -5,11 +5,9 @@ const COORDS = 'coords';
 
 
 function getWeather(lat, lon){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
-    .then(function(response) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(function(response) {
         return response.json();
-    })
-    .then(function(json){
+    }).then(function(json){
         const temperature = json.main.temp;
         const place = json.name;
         weather.innerText = `${temperature} @ ${place}`;
@@ -21,7 +19,7 @@ function saveCoords(coordsObj){
 }
 
 function handleGeoSuccess(position){
-    const latitude = position.coords.latidue;
+    const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const coordsObj = {
         latitude,
